712181df8c7a3077b8565cb917b1ffa7338dfefb
Callum Tolley <cgtrolley@gmail.com>
2020-02-09 19:55:48 +0200

    Fix output dirs

--- ale-sys/build.rs
+++ ale-sys/build.rs
@@ -2,6 +2,7 @@
 use std::io;
 use std::path::{Path, PathBuf};
 use std::fs::{self, DirEntry};
+use std::ffi::OsStr;
 
 #[cfg(windows)]
 fn is_win() -> bool {
@@ -13,13 +14,16 @@ fn is_win() -> bool {
 	false
 }
 
-fn visit_dirs<F: FnMut(&DirEntry)>(dir: &Path, cb: &mut F) -> io::Result<()> {
+fn visit_dirs<F: FnMut(&DirEntry), G: Fn(&DirEntry) -> bool>(dir: &Path, cb: &mut F, filter: &G) -> io::Result<()> {
 	if dir.is_dir() {
 		for entry in fs::read_dir(dir)? {
 			let entry = entry?;
 			let path = entry.path();
+			if !filter(&entry) {
+				continue;
+			}
 			if path.is_dir() {
-				visit_dirs(&path, cb)?;
+				visit_dirs(&path, cb, filter)?;
 			} else {
 				cb(&entry);
 			}
@@ -38,6 +42,13 @@ fn main() {
 		lvl => panic!("Unknown OPT_LEVEL: {}", lvl),
 	};
 
+	let temp_dir = tempdir::TempDir::new("ale-sys-build").expect("failed to create temp dir");
+	println!("temp_dir={}", temp_dir.path().display());
+	let lib_dir = out_dir().join("build").join("lib");
+
+	let cwd = std::env::current_dir().expect("failed to get current dir");
+	std::env::set_current_dir(&temp_dir).expect("failed to set current dir");
+
 	let ale_dir = project_root().join("ale");
 	let mut config = cmake::Config::new(&ale_dir);
 	config
@@ -47,26 +58,34 @@ fn main() {
 		.define("BUILD_CPP_LIB", "OFF")
 		.define("BUILD_CLI", "OFF")
 		.define("BUILD_C_LIB", "ON")
+		.define(format!("CMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}", &profile.to_uppercase()), &lib_dir)
 		.profile(profile)
-		.build_target("ale-c-lib");
+		.build_target("ale-c-lib-static");
 
 	if is_win() {
 		config.cflag("-DWIN32=1").cxxflag("-DWIN32=1");
 	}
 
 	let dst = config.build();
+	std::env::set_current_dir(&cwd).expect("failed to set current dir");
+	println!("dst={}", dst.display());
 
+	let ignore_files: &[&OsStr] = &["build".as_ref(), ".git".as_ref()];
 	let mut ale_files = vec![];
-	visit_dirs(&ale_dir, &mut |de| ale_files.push(de.path())).expect(&format!("visit_dirs failed in {}", ale_dir.display()));
+	visit_dirs(&ale_dir, &mut |de| ale_files.push(de.path()), &|de| de.path().file_name().map(|n| !ignore_files.contains(&n)).unwrap_or(true)).expect(&format!("visit_dirs failed in {}", ale_dir.display()));
 
 	for path in ale_files {
 		println!("cargo:rerun-if-changed={}", path.display());
 	}
 	println!("cargo:rerun-if-changed=build.rs");
-	println!("cargo:rustc-link-search=native={}", dst.join("build").join(profile).display());
-	println!("cargo:rustc-link-lib=static=ale_c");
+	println!("cargo:rustc-link-search=native={}", lib_dir.display());
+	println!("cargo:rustc-link-lib=static=ale_c_static");
 }
 
 fn project_root() -> PathBuf {
 	Path::new(&env!("CARGO_MANIFEST_DIR")).to_path_buf()
+}
+
+fn out_dir() -> PathBuf {
+	Path::new(&std::env::var_os("OUT_DIR").expect("OUT_DIR not defined")).to_path_buf()
 }
