9d707468b36385c949fe197b0b61fbb747b93d3c
Callum Tolley <cgtrolley@gmail.com>
2020-02-11 16:42:13 +0200

    Add basic breakout example (need to polish)

--- examples/breakout.rs
+++ examples/breakout.rs
@@ -3,33 +3,59 @@ use ale::{Ale, BundledRom};
 
 use pixels::{Error, Pixels, SurfaceTexture};
 use winit::dpi::{LogicalPosition, LogicalSize, PhysicalSize};
-use winit::event::{Event, VirtualKeyCode, WindowEvent};
+use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit_input_helper::WinitInputHelper;
 
+const SCREEN_WIDTH: u32 = 160;
+const SCREEN_HEIGHT: u32 = 210;
+
 fn main() -> Result<(), Error> {
-	env_logger::init();
+	let mut ale = Ale::new();
+	ale.load_rom(BundledRom::Breakout).expect("Illegal rom");
+
 	let event_loop = EventLoop::new();
 	let mut input = WinitInputHelper::new();
 	let (window, surface, mut p_width, mut p_height, mut hidpi_factor) =
-		create_window("Conway's Game of Life", &event_loop);
+		create_window("Conway's Game of Life", &event_loop, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	let surface_texture = SurfaceTexture::new(p_width, p_height, surface);
 
-	let mut life = ConwayGrid::new_random(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize);
 	let mut pixels = Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture)?;
 	let mut paused = false;
+	println!("Paused: false");
 
-	let mut draw_state: Option<bool> = None;
+	let mut screen = vec![];
+	screen.resize(SCREEN_WIDTH as usize * SCREEN_HEIGHT as usize * 3, 0u8);
 
 	event_loop.run(move |event, _, control_flow| {
 		// The one and only event that winit_input_helper doesn't have for us...
-		if let Event::WindowEvent {
-			event: WindowEvent::RedrawRequested,
-			..
-		} = event
-		{
-			life.draw(pixels.get_frame());
+		if let Event::RedrawRequested(_) = event {
+			let screen_width = ale.screen_width();
+			let screen_height = ale.screen_height();
+			screen.resize(screen_width * screen_height * 3, 0);
+			ale.get_screen_rgb(&mut screen);
+
+			let frame = pixels.get_frame();
+			let mut x = 0;
+			let mut y = 0;
+			for pixel in frame.chunks_exact_mut(4) {
+				if x < screen_width && y < screen_height {
+					pixel[0] = screen[(y * screen_width + x) as usize * 3    ]; // R
+					pixel[1] = screen[(y * screen_width + x) as usize * 3 + 1]; // G
+					pixel[2] = screen[(y * screen_width + x) as usize * 3 + 2]; // B
+				} else {
+					pixel[0] = 0xff; // R
+					pixel[1] = 0x00; // G
+					pixel[2] = 0x00; // B
+				}
+				pixel[3] = 0xff; // A
+				x += 1;
+				if x >= screen_width {
+					x = 0;
+					y += 1;
+				}
+			}
 			pixels.render();
 		}
 
@@ -43,36 +69,50 @@ fn main() -> Result<(), Error> {
 			}
 			if input.key_pressed(VirtualKeyCode::P) {
 				paused = !paused;
+				println!("Paused: {}", paused);
+			}
+			if input.key_pressed(VirtualKeyCode::R) {
+				ale.reset_game();
+				println!("RESET");
+			}
+			if ale.is_game_over() {
+				println!("Game OVER");
+				ale.reset_game();
+				println!("RESET");
 			}
 			
-			let action = if input.key_pressed(VirtualKeyCode::Left) || input.key_pressed(VirtualKeyCode::A) {
-				action = Some(1);
-			} else if input.key_pressed(VirtualKeyCode::Right) || input.key_pressed(VirtualKeyCode::D) {
-				action = Some(2);
-			} else if input.key_pressed(VirtualKeyCode::Space) {
-				action = Some(3);
+			// Get action
+			let action = if input.key_held(VirtualKeyCode::Left) || input.key_held(VirtualKeyCode::A) {
+				Some(4)
+			} else if input.key_held(VirtualKeyCode::Right) || input.key_held(VirtualKeyCode::D) {
+				Some(3)
+			} else if input.key_held(VirtualKeyCode::Space) {
+				Some(1)
 			} else if !paused {
-				action = Some(0);
+				Some(0)
 			} else {
 				None
 			};
-
+			
+			// Act
+			if let Some(action) = action {
+				if ale.legal_action_set().contains(&action) {
+					//println!("Update: {}", action);
+					ale.act(action);
+				} else {
+					println!("Warning: illegal action: {}", action);
+				}
+			}
+			
 			// Adjust high DPI factor
-			if let Some(factor) = input.hidpi_changed() {
+			if let Some(factor) = input.scale_factor_changed() {
 				hidpi_factor = factor;
 			}
 			// Resize the window
 			if let Some(size) = input.window_resized() {
-				let size = size.to_physical(hidpi_factor);
-				p_width = size.width.round() as u32;
-				p_height = size.height.round() as u32;
+				p_width = size.width;
+				p_height = size.height;
 				pixels.resize(p_width, p_height);
-			}
-			// Act
-			if let Some(action) = action {
-				if ale.legal_action_set().contains(action) {
-					ale.act(action);
-				}
 			}
 			// Request redraw
 			window.request_redraw();
@@ -100,19 +140,19 @@ fn create_window(
 		.with_title(title)
 		.build(&event_loop)
 		.unwrap();
-	let hidpi_factor = window.hidpi_factor();
+	let hidpi_factor = window.scale_factor();
 
 	// Get dimensions
 	let width = width as f64;
 	let height = height as f64;
 	let (monitor_width, monitor_height) = {
 		let size = window.current_monitor().size();
-		(size.width / hidpi_factor, size.height / hidpi_factor)
+		(size.width as f64 / hidpi_factor, size.height as f64 / hidpi_factor)
 	};
 	let scale = (monitor_height / height * 2.0 / 3.0).round();
 
 	// Resize, center, and display the window
-	let min_size = PhysicalSize::new(width, height).to_logical(hidpi_factor);
+	let min_size: winit::dpi::LogicalSize<f64> = PhysicalSize::new(width, height).to_logical(hidpi_factor);
 	let default_size = LogicalSize::new(width * scale, height * scale);
 	let center = LogicalPosition::new(
 		(monitor_width - width * scale) / 2.0,
@@ -124,7 +164,7 @@ fn create_window(
 	window.set_visible(true);
 
 	let surface = pixels::wgpu::Surface::create(&window);
-	let size = default_size.to_physical(hidpi_factor);
+	let size: winit::dpi::PhysicalSize<f64> = default_size.to_physical(hidpi_factor);
 
 	(
 		window,
